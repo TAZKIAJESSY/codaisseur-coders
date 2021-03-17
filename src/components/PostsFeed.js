e93dadbe47683362bdda0a1424fdc8b614283594
tazkiajessy <jessy566eee@gmail.com>
2021-03-17 14:23:37 +0100

    postfeed:2)using redux state

--- src/components/PostsFeed.js
+++ src/components/PostsFeed.js
@@ -1,22 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import moment from "moment";
+
+import { startLoading, postsFetched } from "../store/feed/actions";
+import { selectFeedLoading, selectFeedPosts } from "../store/feed/selectors";
 
 const API_URL = `https://codaisseur-coders-network.herokuapp.com`;
 
 export default function PostFeed() {
-  const [getData, setgetData] = useState({ loading: true, posts: [] });
+  //const [getData, setgetData] = useState({ loading: true, posts: [] });
+
+  const dispatch = useDispatch();
+  const loading = useSelector(selectFeedLoading);
+  const posts = useSelector(selectFeedPosts);
 
   async function fetchNext5Posts() {
-    setgetData({ ...getData, loading: true });
+    //setgetData({ ...getData, loading: true });
+    dispatch(startLoading);
 
     const response = await axios.get(
-      `${API_URL}/posts?offset=${getData.posts.length}&limit=5`
+      `${API_URL}/posts?offset=${posts.length}&limit=5`
     );
     console.log("SOme data fetch", response);
     const morePosts = response.data.rows;
 
-    setgetData({ loading: false, posts: [...getData.posts, ...morePosts] });
+    //setgetData({ loading: false, posts: [...getData.posts, ...morePosts] });
+    dispatch(postsFetched(morePosts));
   }
 
   useEffect(() => {
@@ -26,7 +36,7 @@ export default function PostFeed() {
   return (
     <div>
       <h2>REcent Posts</h2>
-      {getData.posts.map((p, index) => {
+      {posts.map((p, index) => {
         return (
           <div key={index}>
             <h3>{p.title}</h3>
@@ -47,7 +57,7 @@ export default function PostFeed() {
       })}
 
       <p>
-        {getData.loading ? (
+        {loading ? (
           <em>Loading....</em>
         ) : (
           <button onClick={fetchNext5Posts}>Load more</button>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
+import feedSliceReducer from "./feed/reducer";
 // import someFeatureReducer from "./someFeature/reducer";
 
-function someFeatureReducer(state = "hi", action) {
-  return state;
-}
+// function someFeatureReducer(state = "hi", action) {
+//   return state;
+// }
 
 const reducer = combineReducers({
-  someFeature: someFeatureReducer,
+  //someFeature: someFeatureReducer,
+  feed: feedSliceReducer,
 });
 
 export default reducer;
